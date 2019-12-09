d360931848a91f91852d9d33b44b290a599cb9c0
GElrick\HPC <gelrick@parlaygroup.com>
2019-12-09 00:03:31 -0500

    First Commit

---
+++ Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public GameObject target;
+    public Vector2 verticalOffset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(
+                    0,
+                    target.transform.position.y + verticalOffset.y,
+                    transform.position.z
+                );
+        }
+    }
+}
